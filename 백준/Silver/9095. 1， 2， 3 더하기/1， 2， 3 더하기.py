076e4438a59a00b13ca22856a6d3045324e90798
JAEHYEONG8121 <93919966+JAEHYEONG8121@users.noreply.github.com>
2023-08-09 14:42:15 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+l = [0] * 12
+l[1] = 1
+l[2] = 2
+l[3] = 4
+
+for i in range(4, 12) :
+    l[i] = l[i-1] + l[i-2] + l[i-3]
+    
+for _ in range(int(input())) :
+    n = int(input())
+    print(l[n])
+    
